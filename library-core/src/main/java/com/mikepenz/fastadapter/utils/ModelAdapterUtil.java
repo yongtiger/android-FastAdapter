948d865b8b29edbf9236e5898cfee7d01cfe91d6
Robert Han <brainbook.cc@outlook.com>
2021-01-23 16:50:36 -0800

    v1.0.42#typo

--- library-core/src/main/java/com/mikepenz/fastadapter/utils/ModelAdapterUtil.java
+++ library-core/src/main/java/com/mikepenz/fastadapter/utils/ModelAdapterUtil.java
@@ -11,6 +11,7 @@ import java.util.List;
 import static java.util.Arrays.asList;
 
 public abstract class ModelAdapterUtil {
+
     public static <Item extends IItem> List<Item> getAdapterOriginalItems(@Nullable ModelAdapter<Item, Item> adapter) {
         if (adapter == null) {
             return null;
