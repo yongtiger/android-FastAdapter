6a75010d59bd611d72d8625a8707f04b61af4ad7
Robert Han <brainbook.cc@outlook.com>
2021-05-17 22:42:40 -0700

    v1.2.2#[FIX#UIUtils.setBackground]

--- app/src/main/java/com/mikepenz/fastadapter/app/items/SimpleItem.java
+++ app/src/main/java/com/mikepenz/fastadapter/app/items/SimpleItem.java
@@ -107,26 +107,19 @@ public class SimpleItem extends AbstractItem<SimpleItem, SimpleItem.ViewHolder> 
             super(view);
             ButterKnife.bind(this, view);
             this.view = view;
+        }
+
+        @Override
+        public void bindView(@NonNull SimpleItem item, @NonNull List<Object> payloads) {
+            itemView.setSelected(item.isSelected());
 
             ///[FIX#UIUtils.setBackground]
             //get the context
             Context ctx = itemView.getContext();
 
             //set the background for the item
-//            UIUtils.setBackground(itemView, FastAdapterUIUtils.getSelectableBackground(ctx, Color.RED, true));
-            ViewCompat.setBackground(itemView, FastAdapterUIUtils.getSelectableBackground(ctx, Color.RED, true));
-        }
-
-        @Override
-        public void bindView(@NonNull SimpleItem item, @NonNull List<Object> payloads) {
-            ///[FIX#UIUtils.setBackground]
-            itemView.setSelected(item.isSelected());
-//            //get the context
-//            Context ctx = itemView.getContext();
-//
-//            //set the background for the item
-////            UIUtils.setBackground(view, FastAdapterUIUtils.getSelectableBackground(ctx, Color.RED, true));
-//            ViewCompat.setBackground(view, FastAdapterUIUtils.getSelectableBackground(ctx, Color.RED, true));
+//            UIUtils.setBackground(view, FastAdapterUIUtils.getSelectableBackground(ctx, Color.RED, true));
+            ViewCompat.setBackground(view, FastAdapterUIUtils.getSelectableBackground(ctx, Color.RED, true));
 
             //set the text for the name
             StringHolder.applyTo(item.name, name);
